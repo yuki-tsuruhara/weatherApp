998e7b14fb0c9bcb98fb2e0400ce315fd859d696
yuki-tsuruhara <tururin1128@gmail.com>
2023-07-19 23:18:52 +0900

    setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,10 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  globals: {
+    module: true
+  },
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
