f57441aa7600d4b7d38fb97ac6ab5a2a06ebbba4
yuki-tsuruhara <tururin1128@gmail.com>
2023-08-24 22:35:01 +0900

    インポート方式を変更

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,19 +6,19 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: () => import('@/views/Home.vue'),
+      component: () => import('../views/Home.vue'),
       children: [
         {
           path: 'dashboard',
           name: 'dashboard',
-          component: () => import('@/components/Dashboard.vue')
+          component: () => import('../components/Dashboard.vue')
         }
       ]
     },
     {
       path: '/login',
       name: 'login',
-      component: () => import('@/views/Login.vue')
+      component: () => import('../views/Login.vue')
     }
   ]
 })
