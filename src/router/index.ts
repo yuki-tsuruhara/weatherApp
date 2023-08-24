e6cdde63100254897c7a7ea22eadaaca1519eff7
yuki-tsuruhara <tururin1128@gmail.com>
2023-08-24 23:10:11 +0900

    インポート形式を変更

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import Home from '@/views/Home.vue';
+import Login from '@/views/Home.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -6,12 +8,12 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: () => import('../views/Home.vue')
+      component: Home
     },
     {
       path: '/login',
       name: 'login',
-      component: () => import('../views/Login.vue')
+      component: Login
     }
   ]
 });
