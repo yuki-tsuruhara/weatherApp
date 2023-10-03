f9303418f59cee6af42f34d6994adf4fe92bddaa
yuki-tsuruhara <tururin1128@gmail.com>
2023-10-03 23:33:24 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,5 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  }
+  plugins: [vue(), vueJsx()]
 });
