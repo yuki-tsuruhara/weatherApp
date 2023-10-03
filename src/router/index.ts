181524561afe23a3d26657facd068988f6c63c78
yuki-tsuruhara <tururin1128@gmail.com>
2023-10-03 22:42:36 +0900

    読み込みを変更

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
-import Login from '@/views/Home.vue';
+import Login from '@/views/Login.vue';
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
